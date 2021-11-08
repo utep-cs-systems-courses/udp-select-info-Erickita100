1f926a0e56159f65b0f146abef46f407b602f8d9
Erickita100 <Najera-e@att.net>
2021-11-07 22:54:25 -0700

    created select server for file transfer, added RTT, added resending packet is not ack, and stop is more than 3 attempts

--- protocol/client.py
+++ protocol/client.py
@@ -1,44 +1,107 @@
-#! /usr//bin/env python3
-# udp demo client.  Modified from Kurose/Ross by Eric Freudenthal 2016
-
 from socket import *
+import sys, re   
+import os     
+from time import time
 
-
-# default params
+seperator = ";"
+BUFFER_SIZE = 20 # send 4096 bytes each time step
+ACK_TEXT = 'packet_received:'
+ # default params
 serverAddr = ('localhost', 50000)       
 
-import sys, re                          
+                         
+# the name of file we want to send, make sure it exists
+filename = "Data.txt"
+# get the file size
+filesize = os.path.getsize(filename)
+ 
+ #get number of packets, 8 bytes each
+numOfPackets = int(filesize/BUFFER_SIZE)
+
 
 def usage():
-    print("usage: %s [--serverAddr host:port]"  % sys.argv[0])
-    sys.exit(1)
+     print("usage: %s [--serverAddr host:port]"  % sys.argv[0])
+     sys.exit(1)
 
 try:
-    args = sys.argv[1:]
-    while args:
-        sw = args[0]; del args[0]
-        if sw in ("--serverAddr", "-s"):
-            addr, port = re.split(":", args[0]); del args[0]
-            serverAddr = (addr, int(port)) # addr can be a string (yippie)
-        else:
-            print("unexpected parameter %s" % args[0])
-            usage();
+     args = sys.argv[1:]
+     while args:
+         sw = args[0]; del args[0]
+         if sw in ("--serverAddr", "-s"):
+             addr, port = re.split(":", args[0]); del args[0]
+             serverAddr = (addr, int(port)) # addr can be a string (yippie)
+         else:
+             print("unexpected parameter %s" % args[0])
+             usage();
 except:
-    usage()
+     usage()
 
 
-print("serverAddr = %s" % repr(serverAddr))
+print("Connecting to serverAddr = %s" % repr(serverAddr))
+
+header =  str(filesize) + seperator + filename + seperator + str(numOfPackets)
+
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = "hello server"
-clientSocket.sendto(message.encode(), serverAddr)
-modifiedMessage, serverAddrPort = clientSocket.recvfrom(2048)
 
+clientSocket.sendto(header.encode(), serverAddr)
+#modifiedMessage, serverAddrPort = clientSocket.recvfrom(2048)
+#print('Modified message from %s is "%s"' % (repr(serverAddrPort), modifiedMessage.decode()))
 
-if (serverAddr[1]== 50001):
-	print("Input uppercase msg")
+encodedAckText, serverAddrPort  = clientSocket.recvfrom(BUFFER_SIZE)
+ackText = encodedAckText.decode('utf-8')
 
-message = sys.stdin.readline()[:-1]     # delete final \n
-clientSocket.sendto(message.encode(), serverAddr)
-modifiedMessage, serverAddrPort = clientSocket.recvfrom(2048)
-print('Modified message from %s is "%s"' % (repr(serverAddrPort), modifiedMessage.decode()))
+# log if acknowledgment was successful
+if ackText == (ACK_TEXT+str(0)):
+   print('server acknowledged reception of packet:'+str(0))
+else:
+   print('error: server has sent back ' + ackText)
+   
+# start sending the file
+with open(filename, "rb") as f:
+    counter = 1
+    attempts = 1
+    RTT = 0
+    while attempts<3:
+        
+        # read the bytes from the file
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            # file transmitting is done
+            break
+        # we use sendall to assure transimission in 
+        # busy networks
+        start = time()
+        clientSocket.sendto(bytes_read,serverAddr)
+        
+        encodedAckText, serverAddrPrt = clientSocket.recvfrom(BUFFER_SIZE)
+        ackText = encodedAckText.decode('utf-8')
+        
+        
+        received = True
+        while received:
+          # log if acknowledgment was successful
+          if ackText == (ACK_TEXT+str(counter)):
+             print('server acknowledged reception of packet:'+str(counter))
+             
+             RTT = time() - start
+             print('RTT= '+str(RTT))
+             received = False
+          else:
+             print('error: server has not sent back packet:' + str(counter))
+             if (attempts>=3):
+             	print('Number of attempts succeeded, end connection')
+             	break
+             else: 
+                print('Re-sending packet:' + str(counter))
+                clientSocket.sendto(bytes_read,serverAddr)
+                encodedAckText, serverAddrPrt = clientSocket.recvfrom(BUFFER_SIZE)
+                ackText = encodedAckText.decode('utf-8')
+                attempts= attempts + 1
+        counter = counter+1
+   
+     
+print("File Transfer Complete.")   
+        
+# close the socket
+clientSocket.close()   
--- protocol/server.py
+++ protocol/server.py
@@ -1,47 +1,96 @@
-#! /usr/bin/env python3
-# udp demo -- simple select-driven uppercase server
-
-# Eric Freudenthal with mods by Adrian Veliz
-
 import sys
 from socket import *
 from select import select
+import os
+import time
 
-serverAddr= ("", 50000)   # any addr, port 50,000
+upperServerAddr = ("", 50000)   # any addr, port 50,000
+
+# receive 4096 bytes each time
+BUFFER_SIZE = 20
+seperator = ";"
+ACK_TEXT = 'packet_received:'
+filename = " " 
+clientAddrPort = " "
+counter = 0
+
+def receiveHeader(sock):
+   header,clientAddrPort = sock.recvfrom(BUFFER_SIZE)
+   header = header.decode()
+   global filename
+   global counter
+   filesize, filename, numOfPackets = header.split(seperator)
+   # remove absolute path if there is
+   filename = os.path.basename(filename)
+   #adds a 1 to filename to distinguish file transfered
+   filename, format = filename.split(".");
+   filename = filename+ "1."+format
+   # convert to integer
+   filesize = int(filesize)
+   numOfPackets = int(numOfPackets)
+   # now time to send the acknowledgement
+   # encode the acknowledgement text
+   encodedAckText = str(ACK_TEXT+str(counter))
+   # send the encoded acknowledgement text
+   sock.sendto(encodedAckText.encode(),clientAddrPort)
+   print("send ack to header packet:", counter)
+   '''
+   reading = True
+   while reading:
+        time.sleep(1)
+        reading = receiveFile(sock)'''
+   readSockFunc[upperServerSocket] = receiveFile
+
+def receiveFile(sock):
+   # start receiving the file from the socket
+   # and writing to the file stream
+   global counter
+   global filename
+   counter = counter + 1
+   with open(filename, "ab") as f:
+       
+       ACK_TEXT = 'packet_received:'
+        # read 1024 bytes from the socket (receive)
+       bytes_read, clientAddrPort = sock.recvfrom(BUFFER_SIZE)
+           
+       if not bytes_read:    
+            # nothing is received
+            # file transmitting is done
+           return False
+            # write to the file the bytes we just received
+       f.write(str(bytes_read))
+       f.close()
+           # now time to send the acknowledgement for each packet
+           # encode the acknowledgement text
+       encodedAckText = str(ACK_TEXT+ str(counter))
+           # send the encoded acknowledgement text
+       sock.sendto(encodedAckText,clientAddrPort)
+       print("send ack to packet:", counter)
+       #counter = counter+1
+        
 
 
-def welcome(sock):
-  "run this function when sock has rec'd a message"
-  message, clientAddrPort = sock.recvfrom(2048)
-  print("from %s: rec'd '%s'" % (repr(clientAddrPort), message))
-  modifiedMessage = "Hello Client";
-  sock.sendto(modifiedMessage, clientAddrPort)
+upperServerSocket = socket(AF_INET, SOCK_DGRAM)
+upperServerSocket.bind(upperServerAddr)
+upperServerSocket.setblocking(False)
 
-  
-server = socket(AF_INET, SOCK_DGRAM)
-server.bind(serverAddr)
-server.setblocking(False)
-
-
-# map socket to function to call when socket is....
+ # map socket to function to call when socket is....
 readSockFunc = {}               # ready for reading
 writeSockFunc = {}              # ready for writing
 errorSockFunc = {}              # broken
 
 timeout = 5                     # select delay before giving up, in seconds
 
-# function to call when upperServerSocket is ready for reading
-readSockFunc[socket] = welcome
-
+ # function to call when upperServerSocket is ready for reading
+readSockFunc[upperServerSocket] = receiveHeader
 
 print("ready to receive")
 while 1:
-  readRdySet, writeRdySet, errorRdySet = select(list(readSockFunc.keys()),
-                                                list(writeSockFunc.keys()), 
-                                                list(errorSockFunc.keys()),
-                                                timeout)
-  if not readRdySet and not writeRdySet and not errorRdySet:
-    print("timeout: no events")
-  for sock in readRdySet:
-    readSockFunc[sock](sock)
-
+   readRdySet, writeRdySet, errorRdySet = select(list(readSockFunc.keys()),
+                                                 list(writeSockFunc.keys()), 
+                                                 list(errorSockFunc.keys()),
+                                                 timeout)
+   if not readRdySet and not writeRdySet and not errorRdySet:
+     print("timeout: no events")
+   for sock in readRdySet:
+     readSockFunc[sock](sock)
